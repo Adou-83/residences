820698e7dfc4242e10d57e7edfd31b06fdc7b728
Adou-83 <adluc1983@gmail.com>
2026-07-08 14:15:55 +0000

    Configuration Render

--- residences/settings.py
+++ residences/settings.py
@@ -1,18 +1,28 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # =========================
 # SECURITY
 # =========================
 
-SECRET_KEY = 'django-insecure-^c&$ck^*e8l1o4w1%i+bq*ijh#j85734-5n#wdox=n6d+@f3_$'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-^c&$ck^*e8l1o4w1%i+bq*ijh#j85734-5n#wdox=n6d+@f3_$"
+)
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
 
 # =========================
 # APPLICATIONS
@@ -25,9 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'whitenoise.runserver_nostatic',
+
     'core',
 ]
-
 
 # =========================
 # MIDDLEWARE
@@ -35,6 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -43,9 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'residences.urls'
-
 
 # =========================
 # TEMPLATES
@@ -61,43 +73,44 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                 "core.context_processors.global_settings", 
+                'core.context_processors.global_settings',
             ],
         },
     },
 ]
 
-
 WSGI_APPLICATION = 'residences.wsgi.application'
-
 
 # =========================
 # DATABASE
 # =========================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'residences_db',
-        'USER': 'postgres',
-        'PASSWORD': 'seigneur123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
-
 # =========================
-# AUTH PASSWORDS
+# PASSWORD VALIDATION
 # =========================
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
-
 
 # =========================
 # LOCALISATION
@@ -111,20 +124,27 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # =========================
-# STATIC & MEDIA
+# STATIC
 # =========================
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'core/static',
+    BASE_DIR / 'core' / 'static',
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# =========================
+# MEDIA
+# =========================
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # =========================
 # DEFAULT PK
@@ -132,19 +152,21 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # =========================
-# AUTH REDIRECTION
+# LOGIN
 # =========================
 
 LOGIN_URL = 'connexion'
 LOGIN_REDIRECT_URL = 'accueil'
 
-
 # =========================
-# EMAIL (DEV MODE)
+# EMAIL
 # =========================
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# =========================
+# WHATSAPP
+# =========================
 
 WHATSAPP_NUMBER = "2250778485274"
