4fad51982d884634c8d8615b08405ff07768422e
Adou-83 <adluc1983@gmail.com>
2026-08-27 18:02:27 +0000

    Fix allowed hosts for production domain

--- residences/settings.py
+++ residences/settings.py
@@ -16,10 +16,13 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost,residences.onrender.com,residenceslamcy.com,www.residenceslamcy.com"
-).split(",")
+ ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "residences.onrender.com",
+    "residenceslamcy.com",
+    "www.residenceslamcy.com",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://residences.onrender.com",
